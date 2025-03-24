8644d901d9f9213e872d89f8471f9a334504de73
mioriaty <mioriaty@gmail.com>
2025-03-24 20:01:20 +0700

    fix: add default locale getLinkTable

--- src/libs/utils/get-table-link.ts
+++ src/libs/utils/get-table-link.ts
@@ -1,5 +1,6 @@
 import envConfig from '@/configs/env.config';
+import { defaultLocale } from '@/libs/constants/locale';
 
 export const getTableLink = ({ token, tableNumber }: { token: string; tableNumber: number }) => {
-  return envConfig.NEXT_PUBLIC_URL + '/tables/' + tableNumber + '?token=' + token;
+  return `${envConfig.NEXT_PUBLIC_URL}/${defaultLocale}/tables/${tableNumber}?token=${token}`;
 };
